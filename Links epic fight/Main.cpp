da3760d2db0ac9387121f7678218c3a947188fee
MarvFM34 <marvinmeusel@web.de>
2017-03-21 01:54:54 +0100

    moved main loop to class Game

--- Links epic fight/Main.cpp
+++ Links epic fight/Main.cpp
@@ -1,45 +1,13 @@
 #include <SDL2/SDL.h>
 #include <string>
 
+#include "Game.h"
 #include "Rect.h"
 
 int main(){
     SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window *win = SDL_CreateWindow("The Legend of Zelda: Links epic Fight", 100, 100, 1200, 900, SDL_WINDOW_SHOWN);
-    SDL_Surface *surf = SDL_GetWindowSurface(win);
-    SDL_Renderer *r = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
-
-    SDL_Event e;
-    bool quit = false;
-
-    Rect hero = Rect(std::string("Link"), 50, 50, 20, 20, 0, 0, 255);
-    Rect enemy = Rect(std::string("Ganon"), 400, 400, 20, 20, 255, 0, 0);
-
-    while(!quit){
-        SDL_SetRenderDrawColor(r, 0, 255, 0, 255);
-        SDL_RenderClear(r);
-        int speed = 2;
-        while(SDL_PollEvent(&e)){
-            if(e.type == SDL_QUIT) quit = true;
-
-        }
-
-        const Uint8 *state = SDL_GetKeyboardState(NULL);
-        if(state[SDL_SCANCODE_LSHIFT]) speed *= 2;
-        if(state[SDL_SCANCODE_W]) hero.move(0, -speed);
-        if(state[SDL_SCANCODE_A]) hero.move(-speed,0);
-        if(state[SDL_SCANCODE_S]) hero.move(0,speed);
-        if(state[SDL_SCANCODE_D]) hero.move(speed,0);
-        if(hero.collides(&enemy)) enemy.kill();
-
-        if(enemy.isAlive) enemy.draw(r);
-        hero.draw(r);
-
-        SDL_RenderPresent(r);
-    }
-
+    Game game = Game();
+    game.Run();
     SDL_Quit();
     return 0;
-
 }
--- Links epic fight/include/Rect.h
+++ Links epic fight/include/Rect.h
@@ -6,22 +6,22 @@
 class Rect
 {
     public:
-        int xPos;
-        int yPos;
-        int height;
-        int width;
         bool isAlive;
         int red;
         int green;
         int blue;
 
-        std::string name;
         Rect(std::string name, int x, int y, int height, int width, int r, int g, int b);
         void draw(SDL_Renderer* dst);
         bool move(int xDiff, int yDiff);
         void kill();
         bool collides(Rect *otherRect);
     private:
+        int xPos;
+        int yPos;
+        int height;
+        int width;
+        std::string name;
 };
 
 #endif // RECT_H
